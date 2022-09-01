182961c45af8909e8cf4c255fb7a2f1dad0f7afe
zhendahu <zhendahu@gmail.com>
2022-08-31 19:13:59 -0700

    selenium setup

--- main.py
+++ main.py
@@ -2,18 +2,13 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import unittest
 
-
-# USERNAME = input("Please enter your username: \n")
-# PASSWORD = input("Please enter your password: \n")
+USERNAME = input("Please enter the username: \n")
 
 
 driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, 'q')
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get(f"http://www.instagram.com/{USERNAME}/")
+
+
 driver.close()
