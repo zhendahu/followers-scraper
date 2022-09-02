2ac98559c5a0a498d8df6b91b63ff5631bd8ab81
zhendahu <zhendahu@gmail.com>
2022-09-02 11:53:59 -0700

    added functions

--- main.py
+++ main.py
@@ -7,63 +7,62 @@ from selenium.webdriver.support.wait import WebDriverWait
 import unittest
 import time
 
-USERNAME = input("Please enter your username: \n")
-PASSWORD = input("Please enter your password: \n")
+def login(driver):
+    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, f"input[name='username']")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, f"input[name='password']")))
 
-followers = set()
+    username.clear()
+    username.send_keys(USERNAME)
 
-driver = webdriver.Chrome()
-driver.get(f"http://www.instagram.com")
+    password.clear()
+    password.send_keys(PASSWORD)
 
-username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, f"input[name='username']")))
-password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, f"input[name='password']")))
+    Login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-username.clear()
-username.send_keys(USERNAME)
+    time.sleep(2)
+    not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
 
-password.clear()
-password.send_keys(PASSWORD)
+    time.sleep(2)
+    not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
 
-Login_button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-time.sleep(2)
-not_now = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+def find_followers(driver):
+    followers = set()
+    print("finding followers")
+    followers_list = driver.find_elements(By.CLASS_NAME, "_ab8y")
 
-time.sleep(2)
-not_now2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
+    for i in followers_list:
+        innerText = i.get_attribute("innerText")
 
-time.sleep(2)
+        print(innerText)
+        if(innerText != '.' and innerText != '·') :
+            followers.add(innerText)
 
-driver.get("http://www.instagram.com/zhendawho/followers")
+    print("writing followers")
+    with open('followers.txt', 'a') as file:
+        file.write('\n'.join(followers) + "\n")
 
-time.sleep(2)
+    print("done!")
+    return followers
 
-print("finding followers")
-followers_list = driver.find_elements(By.CLASS_NAME, "_ab8y")
 
-for i in followers_list:
-    print(i.get_attribute("innerText"))
-    followers.add(i.get_attribute("innerText"))
-# //*[@id="f30e37da85e3ac"]/div/div/span/a/span/div
-# //*[@id="f2f05aa2d262fd8"]/div/div/span/a/span/div
+if __name__ == "__main__":
+    USERNAME = input("Please enter your username: \n")
+    PASSWORD = input("Please enter your password: \n")
 
-# #f2f05aa2d262fd8 > div > div > span > a > span > div
-#f15a1cfac096eec > div > div > span > a > span > div
+    followers = set()
+    driver = webdriver.Chrome()
+    driver.get(f"http://www.instagram.com")
 
-print("parsing followers")
-for i in followers_list:
-    if i.get_attribute('href'):
-        followers.add(i.get_attribute('href').split("/")[3])
-    else:
-        continue
+    login(driver)
 
-print("writing followers")
-with open('followers.txt', 'a') as file:
-    file.write('\n'.join(followers) + "\n")
+    time.sleep(2)
 
-print("done!")
-# <div class=" _ab8y  _ab94 _ab97 _ab9f _ab9k _ab9p _abcm">anuj.the.manuj</div>
+    driver.get("http://www.instagram.com/zhendawho/followers")
 
-# for i in followers_list:
-#     print(i)
+    time.sleep(2)
+
+    followers = find_followers(driver)
+    
+    # <div class=" _ab8y  _ab94 _ab97 _ab9f _ab9k _ab9p _abcm">anuj.the.manuj</div>
 
